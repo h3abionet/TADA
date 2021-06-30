ebd0914f6f241b11c82c971ffa4ef10d14e302fa
Wojtek Bazant <wbazant@vet.upenn.edu>
2021-06-30 05:40:05 -0400

    Accept parameters for setDadaOpt
    params.dadaOpt can be specified as a dictionary in a config
    or as --dadaOpt.XXX VALUE
    ( providing nested scope params from command line requires Nextflow 20.11.0)
    
    The params are passed to scripts where learnErrors() or dada() are called.
    
    A default dadaOpt = [] removes a startup warning. Deprecate a similar dadaParams in pacbio.nf

--- bin/MergePairs.R
+++ bin/MergePairs.R
@@ -11,10 +11,12 @@ option_list = list(
     make_option(c("--minOverlap"), type="numeric", help="Min overlap"),
     make_option(c("--maxMismatch"), type="numeric", help="Max mismatch"),
     make_option(c("--trimOverhang"), default=FALSE, action="store_true", help="Trim overhanging sequence if overlapping"),
-    make_option(c("--justConcatenate"), default=FALSE, action="store_true", help="Just concatenate sequences")
+    make_option(c("--justConcatenate"), default=FALSE, action="store_true", help="Just concatenate sequences"),
+    make_option(c("--dadaOptStr"), type="character", default='', help="values for setDadaOpt passed as one string")
 )
 
 opt <- parse_args(OptionParser(option_list=option_list))
+eval(parse(text=paste0("setDadaOpt(", opt$dadaOptStr, ")")))
 
 environment(mergePairsRescue) <- asNamespace('dada2')
 
