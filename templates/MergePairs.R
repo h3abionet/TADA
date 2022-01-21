6b14e4ff089651da72e9191bba5261282e60d0e3
Chris Fields <cjfields@illinois.edu>
2022-01-20 23:40:02 -0600

    fix errors with new combined template (need to escape the $ in code template)

--- templates/MergePairs.R
+++ templates/MergePairs.R
@@ -1,7 +1,7 @@
 #!/usr/bin/env Rscript
 suppressPackageStartupMessages(library(dada2))
 
-rescuePairs <- as.logical(${params.rescueUnmerged})
+rescuePairs <- as.logical("${params.rescueUnmerged}")
 
 mergePairsRescue <- function(dadaF, derepF, dadaR, derepR,
                         minOverlap = 12, maxMismatch=0, returnRejects=FALSE,
@@ -25,21 +25,21 @@ mergePairsRescue <- function(dadaF, derepF, dadaR, derepR,
    if(length(unique(nrecs))>1) stop("The dadaF/derepF/dadaR/derepR arguments must be the same length.")
 
    rval <- lapply(seq_along(dadaF), function (i)  {
-   mapF <- getDerep(derepF[[i]])$map
-   mapR <- getDerep(derepR[[i]])$map
-   if(!(is.integer(mapF) && is.integer(mapR))) stop("Incorrect format of $map in derep-class arguments.")
+   mapF <- getDerep(derepF[[i]])\$map
+   mapR <- getDerep(derepR[[i]])\$map
+   if(!(is.integer(mapF) && is.integer(mapR))) stop("Incorrect format of \$map in derep-class arguments.")
    #    if(any(is.na(rF)) || any(is.na(rR))) stop("Non-corresponding maps and dada-outputs.")
    if(!(length(mapF) == length(mapR) && 
-        max(mapF, na.rm=TRUE) == length(dadaF[[i]]$map) &&
-        max(mapR, na.rm=TRUE) == length(dadaR[[i]]$map))) {
+        max(mapF, na.rm=TRUE) == length(dadaF[[i]]\$map) &&
+        max(mapR, na.rm=TRUE) == length(dadaR[[i]]\$map))) {
       stop("Non-corresponding derep-class and dada-class objects.")
    }
-   rF <- dadaF[[i]]$map[mapF]
-   rR <- dadaR[[i]]$map[mapR]
+   rF <- dadaF[[i]]\$map[mapF]
+   rR <- dadaR[[i]]\$map[mapR]
 
    pairdf <- data.frame(sequence = "", abundance=0, forward=rF, reverse=rR)
    ups <- unique(pairdf) # The unique forward/reverse pairs of denoised sequences
-   keep <- !is.na(ups$forward) & !is.na(ups$reverse)
+   keep <- !is.na(ups\$forward) & !is.na(ups\$reverse)
    ups <- ups[keep, ]
    if (nrow(ups)==0) {
       outnames <- c("sequence", "abundance", "forward", "reverse",
@@ -51,17 +51,17 @@ mergePairsRescue <- function(dadaF, derepF, dadaR, derepR,
       }
       return(ups)
     } else {
-      Funqseq <- unname(as.character(dadaF[[i]]$clustering$sequence[ups$forward]))
-      Runqseq <- rc(unname(as.character(dadaR[[i]]$clustering$sequence[ups$reverse])))
+      Funqseq <- unname(as.character(dadaF[[i]]\$clustering\$sequence[ups\$forward]))
+      Runqseq <- rc(unname(as.character(dadaR[[i]]\$clustering\$sequence[ups\$reverse])))
       if (justConcatenate == TRUE) {
          # Simply concatenate the sequences together
-         ups$sequence <- mapply(function(x,y) paste0(x,"NNNNNNNNNN", y),
+         ups\$sequence <- mapply(function(x,y) paste0(x,"NNNNNNNNNN", y),
                                Funqseq, Runqseq, SIMPLIFY=FALSE);
-         ups$nmatch <- 0
-         ups$nmismatch <- 0
-         ups$nindel <- 0
-         ups$prefer <- NA
-         ups$accept <- TRUE
+         ups\$nmatch <- 0
+         ups\$nmismatch <- 0
+         ups\$nindel <- 0
+         ups\$prefer <- NA
+         ups\$accept <- TRUE
       } else {
          # Align forward and reverse reads.
          # Use unbanded N-W align to compare forward/reverse
@@ -75,43 +75,43 @@ mergePairsRescue <- function(dadaF, derepF, dadaR, derepR,
          alvecs <- mapply(function(x,y) nwalign(x,y,band=-1,...), Funqseq, Runqseq, SIMPLIFY=FALSE)
          setDadaOpt(tmp)
          outs <- t(sapply(alvecs, function(x) C_eval_pair(x[1], x[2])))
-         ups$nmatch <- outs[,1]
-         ups$nmismatch <- outs[,2]
-         ups$nindel <- outs[,3]
-         ups$prefer <- 1 + (dadaR[[i]]$clustering$n0[ups$reverse] > dadaF[[i]]$clustering$n0[ups$forward])
-         ups$accept <- (ups$nmatch >= minOverlap) & ((ups$nmismatch + ups$nindel) <= maxMismatch)
+         ups\$nmatch <- outs[,1]
+         ups\$nmismatch <- outs[,2]
+         ups\$nindel <- outs[,3]
+         ups\$prefer <- 1 + (dadaR[[i]]\$clustering\$n0[ups\$reverse] > dadaF[[i]]\$clustering\$n0[ups\$forward])
+         ups\$accept <- (ups\$nmatch >= minOverlap) & ((ups\$nmismatch + ups\$nindel) <= maxMismatch)
          # Make the sequence
-         ups$sequence <- mapply(C_pair_consensus, sapply(alvecs,`[`,1), sapply(alvecs,`[`,2), ups$prefer, trimOverhang);
+         ups\$sequence <- mapply(C_pair_consensus, sapply(alvecs,`[`,1), sapply(alvecs,`[`,2), ups\$prefer, trimOverhang);
          # Additional param to indicate whether 1:forward or 2:reverse takes precedence
          # Must also strip out any indels in the return
          # This function is only used here.
       }
 
       # Add abundance and sequence to the output data.frame
-      tab <- table(pairdf$forward, pairdf$reverse)
-      ups$abundance <- tab[cbind(ups$forward, ups$reverse)]
+      tab <- table(pairdf\$forward, pairdf\$reverse)
+      ups\$abundance <- tab[cbind(ups\$forward, ups\$reverse)]
       if (rescueUnmerged == TRUE) {
-         rescue <- which(!ups$accept)
-         ups$sequence[rescue] <- mapply(function(x,y) paste0(x,"NNNNNNNNNN", y),
+         rescue <- which(!ups\$accept)
+         ups\$sequence[rescue] <- mapply(function(x,y) paste0(x,"NNNNNNNNNN", y),
                                Funqseq[rescue], Runqseq[rescue], SIMPLIFY=FALSE);
       } else {
-         ups$sequence[!ups$accept] <- ""
+         ups\$sequence[!ups\$accept] <- ""
       }
       # Add columns from forward/reverse clustering
-      propagateCol <- propagateCol[propagateCol %in% colnames(dadaF[[i]]$clustering)]
+      propagateCol <- propagateCol[propagateCol %in% colnames(dadaF[[i]]\$clustering)]
       for(col in propagateCol) {
-        ups[,paste0("F.",col)] <- dadaF[[i]]$clustering[ups$forward,col]
-        ups[,paste0("R.",col)] <- dadaR[[i]]$clustering[ups$reverse,col]
+        ups[,paste0("F.",col)] <- dadaF[[i]]\$clustering[ups\$forward,col]
+        ups[,paste0("R.",col)] <- dadaR[[i]]\$clustering[ups\$reverse,col]
       }
       # Sort output by abundance and name
-      ups <- ups[order(ups$abundance, decreasing=TRUE),]
+      ups <- ups[order(ups\$abundance, decreasing=TRUE),]
       rownames(ups) <- NULL
       if(verbose) {
-         message(sum(ups$abundance[ups$accept]), " paired-reads (in ", sum(ups$accept), " unique pairings) successfully merged out of ", sum(ups$abundance), " (in ", nrow(ups), " pairings) input.")
+         message(sum(ups\$abundance[ups\$accept]), " paired-reads (in ", sum(ups\$accept), " unique pairings) successfully merged out of ", sum(ups\$abundance), " (in ", nrow(ups), " pairings) input.")
       }
-      if(!returnRejects) { ups <- ups[ups$accept,] }
+      if(!returnRejects) { ups <- ups[ups\$accept,] }
 
-      if(any(duplicated(ups$sequence))) {
+      if(any(duplicated(ups\$sequence))) {
          message("Duplicate sequences in merged output.")
       }
       return(ups)
