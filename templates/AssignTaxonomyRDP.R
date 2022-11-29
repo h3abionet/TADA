c2829d3c647a8c9a4130c19e96d302313469e72a
Chris Fields <cjfields@illinois.edu>
2022-11-29 10:42:52 -0600

    add batch to non-species assignment script; we should deprecate this and merge into the main assign species code

--- templates/AssignTaxonomyRDP.R
+++ templates/AssignTaxonomyRDP.R
@@ -4,18 +4,53 @@ suppressPackageStartupMessages(library(dada2))
 seqs <- readRDS("${st}")
 
 # Assign taxonomy
-tax <- assignTaxonomy(seqs\$seq, "${ref}",
+tax <- NULL
+boots <- NULL
+
+if ( ${params.taxBatch} == 0 ) { # no batch, run normally
+    cat("Running all samples\n")
+    tax <- assignTaxonomy(seqtab, "${ref}",
+                    multithread=${task.cpus},
+                    tryRC = TRUE,
+                    outputBootstraps = TRUE,
+                    minBoot = ${params.minBoot},
+                    verbose = TRUE)
+
+    boots <- tax\$boot
+} else {
+    # see https://github.com/benjjneb/dada2/issues/1429 for this
+    to_split <- seq(1, ncol(seqtab), by = ${params.taxBatch})
+    to_split2 <- c(to_split[2:length(to_split)]-1, ncol(seqtab))
+
+    for(i in 1:length(to_split)){
+        cat(paste("Running all samples from",to_split[i], "to", to_split2[i], "\n"))
+        seqtab2 <- seqtab[, to_split[i]:to_split2[i]]
+        tax2 <- assignTaxonomy(seqtab2, "${ref}",
                 multithread=${task.cpus},
                 tryRC = TRUE,
                 outputBootstraps = TRUE,
                 minBoot = ${params.minBoot},
                 verbose = TRUE)
 
-boots <- tax\$boot
+        if (is.null(boots)) {
+            boots <- tax2\$boot
+        } else {
+            boots <- rbind(boots, tax2\$boot)
+        }
 
-rownames(tax\$tax) <- seqs[rownames(tax\$tax),]\$id
+        if (is.null(tax)) {
+            tax <- tax2
+        } else {
+            tax <- rbind(tax, tax2)
+        }
+    }
+}
+
+# make sure these are the same order
+# they should be, but we don't assume this
+rownames(tax) <- seqs[rownames(tax),]\$id
 rownames(boots) <- seqs[rownames(boots),]\$id
 
-# Write to disk
-saveRDS(tax\$tax, "tax_final.${seqtype}.RDS")
+# Write original data
+saveRDS(tax, "tax_final.${seqtype}.RDS")
 saveRDS(boots, "bootstrap_final.${seqtype}.RDS")
