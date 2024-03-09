a955f41cf383720dffab2f5bf0107485095294b6
Chris Fields <cjfields@illinois.edu>
2024-03-09 10:48:14 -0600

    this seems to havebeen dropped at some point; the sample IDs in the seq tables aren't the actual sample IDs (largely due to how the tables are set up). Can possibly be fixed early in the pipeline but needs to be tested

--- templates/RenameASVs.R
+++ templates/RenameASVs.R
@@ -29,6 +29,10 @@ writeFasta(seqs.dna, file = 'asvs.${params.idType}.nochim.${seqtype}.fna')
 seqs.dna.raw <- ShortRead(sread = DNAStringSet(seqs.raw), id = BStringSet(ids_study.raw))
 writeFasta(seqs.dna.raw, file = 'asvs.${params.idType}.raw.${seqtype}.fna')
 
+# replace rownames
+rownames(st) <- gsub(".R1.filtered.fastq.gz", "", rownames(st))
+rownames(st.raw) <- gsub(".R1.filtered.fastq.gz", "", rownames(st.raw))
+
 # Write modified data (note we only keep the no-chimera reads for the next stage)
 saveRDS(st, "seqtab_final.${params.idType}.${seqtype}.RDS")
 saveRDS(data.frame(id = ids_study, seq = seqs), "readmap.${seqtype}.RDS")
